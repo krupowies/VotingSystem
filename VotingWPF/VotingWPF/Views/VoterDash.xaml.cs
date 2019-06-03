7244aa4be5b6b2d6a219a111f33fc6ce7886e6ac
krupowies <krupowieswojciech@gmail.com>
2019-06-03 14:16:09 +0200

    added few new views

---
+++ VotingWPF/VotingWPF/Views/VoterDash.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VotingWPF.Views
+{
+    /// <summary>
+    /// Logika interakcji dla klasy VoterDash.xaml
+    /// </summary>
+    public partial class VoterDash : Window
+    {
+        public VoterDash()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseEleButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OtherButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
